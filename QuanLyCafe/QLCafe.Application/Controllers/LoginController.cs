8d8f5c33d8c992c8eaa50df5d3fd704a5c8e3546
ITMan <datoanh25032017@gmai.com>
2020-10-08 02:27:12 +0700

    tạo viewLogin

---
+++ QuanLyCafe/QLCafe.Application/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QLCafe.Application.Controllers
+{
+    public class LoginController : Controller
+    {
+        [Route("Login")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
