bb5785ab2294966e72264add1f0a2817bfb57255
NguyenQuocHoa <nguyenquochoa2000tb2@gmail.com>
2020-10-07 07:23:29 +0700

    Add context

---
+++ QuanLyCafe/QLCafe.Application/Data/QLCafeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLCafe.Application.Data
+{
+    public class QLCafeContext : DbContext
+    {
+        public QLCafeContext(DbContextOptions<QLCafeContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<Name_Model> Name_Model { get; set; }
+    }
+}
